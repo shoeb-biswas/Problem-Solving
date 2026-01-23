f0698b09ee2cbb7e195aea6a63003aabb5187e0e
MD. Shoeb Hossain <shoebbiswas444@gmail.com>
2026-01-23 19:29:50 +0600

    problem 1008
    Write a program that reads an employee's number, his/her worked hours number in a month and the amount he received per hour. Print the employee's number and salary that he/she will receive at end of the month, with two decimal places.
    
    1. Don’t forget to print the line's end after the result, otherwise you will receive “Presentation Error”.
    2. Don’t forget the space before and after the equal signal and after the U$.

---
+++ Salary.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+ int A,B;
+ scanf("%d %d",&A,&B);
+ float C,X;
+ scanf("%f",&C);
+ X=B*C;
+ printf("NUMBER = %d\nSALARY = U$ %.2f\n",A,X);
+ 
+    return 0;
+}
