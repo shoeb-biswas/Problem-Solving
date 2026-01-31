c079689dd3771b860c0095c22e3fc12c32269c78
MD. Shoeb Hossain <shoebbiswas444@gmail.com>
2026-01-31 09:09:07 +0600

    Problem 1064
    Read 6 values that can be floating point numbers. After, print how many of them were positive. In the next line, print the average of all positive values typed, with one digit after the decimal point.

---
+++ Positives and Average.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    float num, sum = 0;
+    int count = 0;
+    for (int i = 0; i < 6; i++) {
+        scanf("%f", &num);
+        if (num > 0) {
+            sum += num;
+            count++;
+        }
+    }
+    printf("%d valores positivos\n", count);
+    printf("%.1f\n", sum / count);
+    return 0;
+}
