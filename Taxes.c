8b160e529773e5a53d150f03e2b100ef25eaca6e
MD. Shoeb Hossain <shoebbiswas444@gmail.com>
2026-02-01 08:16:09 +0600

    Problem 1051
    In an imaginary country called Lisarb, all the people are very happy to pay their taxes because they know that doesn’t exist corrupt politicians and the taxes are used to benefit the population, without any misappropriation. The currency of this country is Rombus, whose symbol is R$.
    
    Read a value with 2 digits after the decimal point, equivalent to the salary of a Lisarb inhabitant. Then print the due value that this person must pay of taxes, according to the table below.
    
    Salary                         | Taxes
    -----------------------------------------
    from 0.00 to R$ 2,000.00       | Without taxes
    from R$ 2,000.01 to R$ 3,000.00| 8 %
    from R$ 3,000.01 to R$ 4,500.00| 18 %
    more than R$ 4,500.00          | 28 %
    
    Remember, if the salary is R$ 3,002.00 for example, the rate of 8% is only over R$ 1,000.00, because the salary from R$ 0.00 to R$ 2,000.00 is tax free. In the follow example, the total rate is 8% over R$ 1000.00 + 18% over R$ 2.00, resulting in R$ 80.36 at all. The answer must be printed with 2 digits after the decimal point.

---
+++ Taxes.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    double salary, tax = 0.0;
+    scanf("%lf", &salary);
+
+    if (salary <= 2000.00) {
+        printf("Isento\n");
+    } else {
+        if (salary > 2000.00 && salary <= 3000.00) {
+            tax += (salary - 2000.00) * 0.08;
+        } else if (salary > 3000.00 && salary <= 4500.00) {
+            tax += (1000.00 * 0.08);
+            tax += (salary - 3000.00) * 0.18;
+        } else if (salary > 4500.00) {
+            tax += (1000.00 * 0.08);
+            tax += (1500.00 * 0.18);
+            tax += (salary - 4500.00) * 0.28;
+        }
+        printf("R$ %.2lf\n", tax);
+    }
+
+    return 0;
+}
