6972be11cc3e89ba71adb6151e167924089cb33a
MD. Shoeb Hossain <shoebbiswas444@gmail.com>
2026-02-03 07:58:29 +0600

    Problem 1042
    Read three integers and sort them in ascending order. After, print these values in ascending order, a blank line and then the values in the sequence as they were readed.

---
+++ Simple Sort.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+int x,y,z,temp;
+scanf("%d%d%d",&x,&y,&z);
+int a=x,b=y,c=z;
+if(x>y){temp=x;x=y; y=temp;}
+if(x>z){temp=x;x=z; z=temp;}
+if(y>z){temp=y;y=z; z=temp;}
+printf("%d\n%d\n%d\n\n",x,y,z);
+printf("%d\n%d\n%d\n",a,b,c);
+    return 0;
+}
