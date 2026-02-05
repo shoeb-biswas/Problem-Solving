d442d5c9c3d5ad8932365dfc7d36ab19ccb9bd3d
MD. Shoeb Hossain <shoebbiswas444@gmail.com>
2026-02-05 10:17:20 +0600

    Problem 1177
    Write a program that reads a number T and fill a vector N[1000] with the numbers from 0 to T-1 repeated times, like as the example below.

---
+++ Array Fill II.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int T;
+    scanf("%d", &T);
+    
+    int N[1000];
+    for (int i = 0; i < 1000; i++) {
+        N[i] = i % T;
+        printf("N[%d] = %d\n", i, N[i]);
+    }
+    
+    return 0;
+}
